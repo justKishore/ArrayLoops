aba980e9fa003526c7d31ac3287eb8806512ca76
Kishore S <skishore1843@gmail.com>
2021-10-06 20:27:04 +0530

    Add till for each loop

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fruits = ['Apple','Banana','Mango','Orange'];
 console.log("-- For Loop --")
 for(var i=0;i<fruits.length;i++){
     console.log("Fruit "+(i+1)+": "+fruits[i]);
-    /*op: Fruit 1: Apple
-          Fruit 2: Banana
-          Fruit 3: Mango
-          Fruit 4: Orange*/
+    /*op: 
+     Fruit 1: Apple
+     Fruit 2: Banana
+     Fruit 3: Mango
+     Fruit 4: Orange
+    */
 }
 
 // 2. While loop
@@ -15,7 +17,99 @@ console.log("-- While Loop --")
 var x = 0;
 while(x<fruits.length){
     console.log("Fruit "+(x+1)+": "+fruits[x]);
+    /* op: 
+     Fruit 1: Apple
+     Fruit 2: Banana
+     Fruit 3: Mango
+     Fruit 4: Orange
+    */
     x++;
 }
 
 // 3. Do  while
+console.log("-- Do While Loop --");
+var x =0;
+do{
+    console.log("Fruit "+(x+1)+": "+fruits[x]);
+    /* op: 
+     Fruit 1: Apple
+     Fruit 2: Banana
+     Fruit 3: Mango
+     Fruit 4: Orange
+    */
+    x++;
+} while(x<fruits.length);
+
+// For in (returns key of array or object)
+var fruitList = "";
+ for ( var i in fruits){
+     console.log("i is: "+i);
+     /* op: 
+     i is: 0
+     i is: 1
+     i is: 2
+     i is: 3
+     */
+    fruitList += i+ ": "+ fruits[i] + "\n";
+ }
+console.log(fruitList);
+/* op:
+ 0: Apple
+ 1: Banana
+ 2: Mango
+ 3: Orange
+*/
+
+var city = {"B":"Berlin","N":"Nairobi","T":"Tokyo"};
+var cityList = "";
+for (var key in city) {
+    cityList += key + ": " + city[key]+ "\n";
+    }
+console.log(cityList);
+/* op:
+ B: Berlin
+ N: Nairobi
+ T: Tokyo
+*/
+
+// For off (returns value of array, map, set and Arguments object)
+for(var value of fruits){
+    console.log("Value is: "+value);
+    /* op:
+     Value is: Apple
+     Value is: Banana
+     Value is: Mango
+     Value is: Orange
+    */
+}
+/*
+for(var value of city){
+    console.log("City is:"+value); // Not possible (city is not iterable)
+}
+*/
+
+// For each (calls a function (a callback function) once for each array element)
+var fruitNewList = "";
+fruits.forEach(fruitFunction);
+console.log("Fruits: \n"+fruitNewList);
+/* op:
+ Fruits: 
+ Apple
+ Banana
+ Mango
+ Orange
+*/
+function fruitFunction(value, index, array) {
+    fruitNewList += value + "\n";
+}
+
+Object.entries(city).forEach(cities => {
+    const [key, value] = cities;
+    console.log(key, value);
+});
+
+/* op:
+ B Berlin
+ N Nairobi
+ T Tokyo
+*/
